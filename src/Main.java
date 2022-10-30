d39617d0a047205f49331d2db2c313c8988955ce
ugurmasat92 <93405719+ugurmasat92@users.noreply.github.com>
2022-10-30 15:33:36 +0300

    mukemmelSayi

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        //6 --> 1, 2, 3
+        //28 --> 1, 2, 4, 7, 14
+        int number = 5;
+        int total = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                total = total + i;
+            }
+        }
+        if (total == number) {
+            System.out.println("Mükemmel sayıdır");
+        } else {
+            System.out.println("Mükemmel sayı değildir");
+        }
+    }
+}
+
+
+// 5 % 1 = 0
+// 5 % 2 = 1
+// 5 % 3 = 2
